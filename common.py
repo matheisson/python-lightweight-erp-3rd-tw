f3fefd31f78d6313f7e50dc1d261f20775d9a171
Shevah <shevah92@gmail.com>
2016-06-13 11:53:10 +0200

    random generated id function added

--- common.py
+++ common.py
@@ -1,6 +1,10 @@
+#!/usr/bin/env python
 # implement commonly used functions here
 
+
 import random
+import ui
+import string
 
 
 # generate and return a unique and random string
@@ -11,9 +15,29 @@ import random
 # @table: list of list
 # @generated: string - generated random string (unique in the @table)
 def generate_random(table):
+    good_pw = [0, 0, 0, 0]
+    chars = string.ascii_letters + string.digits + "[!@#$%^&*()?]"
+    generated = []
+    for i in range(8):
+        generated.append(random.choice(chars))
+    for i in range(8):
+        if generated[i] in string.ascii_lowercase:
+            good_pw[0] += 1
+        elif generated[i] in string.ascii_uppercase:
+            good_pw[1] += 1
+        elif generated[i] in string.digits:
+            good_pw[2] += 1
+        elif generated[i] in "[!@#$%^&*()?]":
+            good_pw[3] += 1
+    generated = "".join(generated)
+    if good_pw == [2, 2, 2, 2]:
+        for i in range(len(table)):
+            if generated not in str(table[i][0]):
+                return generated
+            else:
+                return generate_random(table)
+    else:
+        return generate_random(table)
 
-    generated = ''
-
-    # your code
-
-    return generated
+# table = [[1], [2]]
+# print(generate_random(table))
